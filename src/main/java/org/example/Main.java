e09ba8efd4a112537de4be44d2279d1b0ff9298e
korjje617 <korjje617@gmail.com>
2025-12-10 00:57:24 +0900

    회원가입 및 아이디 중복, 비밀번호 정확성 확인

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     static List<Article> articles = new ArrayList<>();
+    static List<Member> members = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,8 +15,10 @@ public class Main {
         System.out.println("=== Article Manager ===");
 
         makeTestData();
+        makeUserData();
 
         int lastArticleId = 3;
+        int lastMemberId = 3;
 
         while (true) {
             System.out.print("명령어 ) ");
@@ -29,7 +32,48 @@ public class Main {
                 continue;
             }
 
-            if (cmd.equals("article write")) {
+            if (cmd.equals("member join")) {
+                System.out.println("==회원 가입==");
+                int memId = lastMemberId + 1;
+
+                String loginId = null;
+                while (true) {
+                    System.out.print("로그인 아이디 : ");
+                    loginId = sc.nextLine().trim();
+                    if (isJoinableLoginId(loginId) == false) {
+                        System.out.println("이미 사용중인 loginId");
+                        continue;
+                    }
+                    break;
+                }
+
+                String password = null;
+                while (true) {
+                    System.out.print("비밀번호 : ");
+                    password = sc.nextLine().trim();
+                    System.out.print("비밀번호 재확인 : ");
+                    String passwordConfirm = sc.nextLine().trim();
+
+                    if (password.equals(passwordConfirm) == false) {
+                        System.out.println("비밀번호가 다릅니다");
+                        continue;
+                    }
+                    break;
+                }
+
+                System.out.print("이름 : ");
+                String name = sc.nextLine().trim();
+                String regDate = Util.getNowStr();
+                String updateDate = Util.getNowStr();
+
+                Member member = new Member(memId, loginId, password, name, regDate, updateDate);
+                members.add(member);
+
+                System.out.println(memId + "번 회원 가입 완료.");
+                lastMemberId++;
+            }
+
+            else if (cmd.equals("article write")) {
                 System.out.println("==게시글 작성==");
                 int id = lastArticleId + 1;
                 System.out.print("제목 : ");
@@ -151,6 +195,18 @@ public class Main {
     }
 
     /**
+     * 회원 아이디 찾기
+     **/
+    private static boolean isJoinableLoginId(String loginId) {
+        for (Member member : members) {
+            if (member.getLoginId().equals(loginId)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
      * 아이디 찾기
      **/
     private static Article getArticleById(int id) {
@@ -163,13 +219,23 @@ public class Main {
     }
 
     /**
-     * 테스트 데이터
+     * 테스트 게시글
      */
     private static void makeTestData() {
-        System.out.println("테스트 데이터 만들기");
+        System.out.println("테스트 게시글 만들기");
         articles.add(new Article(1, Util.getNowStr(), Util.getNowStr(), "테스트 제목", "테스트 제목"));
         articles.add(new Article(2, Util.getNowStr(), Util.getNowStr(), "테스트 제목", "테스트 제목"));
         articles.add(new Article(3, Util.getNowStr(), Util.getNowStr(), "테스트 제목", "테스트 제목"));
+    }
+
+    /**
+     * 테스트 유저
+     */
+    private static void makeUserData() {
+        System.out.println("테스트 유저 만들기");
+        members.add(new Member(1, "aaa", "aaa", "테스트1", "regDate", "updateDate"));
+        members.add(new Member(2, "bbb", "bbb", "테스트2", "regDate", "updateDate"));
+        members.add(new Member(3, "ccc", "bbb", "테스트3", "regDate", "updateDate"));
     }
 }
 // ---------------------------------------------------------------
@@ -230,4 +296,69 @@ class Article {
     }
 
 
+}
+
+class Member {
+    private int memid;
+    private String loginId;
+    private String password;
+    private String name;
+    private String regDate;
+    private String updateDate;
+
+    public Member(int memid, String loginId, String password, String name, String regDate, String updateDate) {
+        this.memid = memid;
+        this.loginId = loginId;
+        this.password = password;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMemid() {
+        return memid;
+    }
+
+    public void setMemid(int memid) {
+        this.memid = memid;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
 }
